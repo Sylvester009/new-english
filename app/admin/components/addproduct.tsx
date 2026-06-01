f64a42325f93d759693ab6278f4bef85e0a0847c
Samuel Sylvester <sasy260902@gmail.com>
2026-06-01 22:06:47 +0100

    Update addproduct.tsx

--- app/admin/components/addproduct.tsx
+++ app/admin/components/addproduct.tsx
@@ -61,7 +61,7 @@ export default function AddProductForm({
       }
       if (onSuccess) onSuccess();
     } catch (err: any) {
-      console.error(`Error: ${err.message}`);
+      toast.error(`Error: ${err.message}`);
     } finally {
       setLoading(false);
     }
